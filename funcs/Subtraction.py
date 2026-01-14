c42ebfa8c60749c8225b16eced63a6d8717f2617
2779765h <2779765h@student.gla.ac.uk>
2026-01-14 10:41:37 +0000

    Update Subtraction.py

--- funcs/Subtraction.py
+++ funcs/Subtraction.py
@@ -4,4 +4,4 @@ def sub(a,b):
   "function to subtract two numbers (a - b) and output the result"
   return a - b
 
-print('Subtraction of {0} - {1} is :{2}'.format(a, b, sub(a,b)))
+print('Subtraction of {0} - {1} is {2}'.format(a, b, sub(a,b)))

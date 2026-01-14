3a2e2c93a531d673a9832b4021c2bc54dc41596b
2779765h <2779765h@student.gla.ac.uk>
2026-01-14 10:29:36 +0000

    Update Addition.py

--- funcs/Addition.py
+++ funcs/Addition.py
@@ -2,4 +2,4 @@ def add(a,b):
   "function to add two numbers (a + b) and output the sum"
   return a + b
 
-print('Addition of a:{0} + b:{1} is :{2}'.format(a,b,add(a,b)))
+print('Addition of {0} + {1} is :{2}'.format(a,b,add(a,b)))

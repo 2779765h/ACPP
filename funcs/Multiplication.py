d80c10356eb48625560d7652f16d60b9c32f8aa0
2779765h <2779765h@student.gla.ac.uk>
2026-01-14 10:41:27 +0000

    Update Multiplication.py

--- funcs/Multiplication.py
+++ funcs/Multiplication.py
@@ -2,4 +2,4 @@ def product(a,b):
   "function multiplies two numbers (a*b) and outputs the product"
   return a*b
 
-print('Multiplying {0} x {1} gives :{2}'.format(a, b, product(a,b)))
+print('Multiplying {0} x {1} gives {2}'.format(a, b, product(a,b)))

f049edfab25787f32748798751fe2ce6d48b0f96
Alicja <2779765h@student.gla.ac.uk>
2026-01-20 13:37:17 +0000

    Update Addition.py
    Change from " to '''

--- funcs/Addition.py
+++ funcs/Addition.py
@@ -1,5 +1,5 @@
 def add(a,b):
-  "function to add two numbers (a,b) and output the sum (a + b)"
+  '''function to add two numbers (a,b) and output the sum (a + b)'''
   return a + b
 
 print('Addition of {0} + {1} is {2}'.format(a, b, add(a,b)))

6034dc3c611e91588af7f17a2cef23baa0bb0b9e
Alicja <2779765h@student.gla.ac.uk>
2026-02-04 15:03:30 +0000

    Update Pythagoras.py
    Moving print statement inside function, defining data types of variables

--- funcs/Pythagoras.py
+++ funcs/Pythagoras.py
@@ -3,12 +3,12 @@ import numpy as np
 def hypotenuse(a,b):
   '''
   Function calculates the hypotenuse of a right angled triangle
-  
-  
+
+  data types:
+  a: float
+  b: float
+  output: float
   '''
-  return np.sqrt((a**2) + (b**2))
+  print('The hypotenuse of a right-angled triangle with side lengths of {0:.2f} and {1:.2f} is {2:.2f}'.format(a, b, np.sqrt((a**2) + (b**2))))
+  return  np.sqrt((a**2) + (b**2))
 
-print('The hypotenuse of a right-angled triangle with side lengths of {0} and {1} is {2:.f}'.format(a, b, hypotenuse(a,b)))
-
-
-  

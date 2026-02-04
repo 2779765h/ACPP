4ed4521fda26e26373850638bce29b5e546f635b
Alicja <2779765h@student.gla.ac.uk>
2026-02-04 15:05:51 +0000

    Update Pythagoras.py

--- funcs/Pythagoras.py
+++ funcs/Pythagoras.py
@@ -7,7 +7,7 @@ def hypotenuse(a,b):
   data types:
   a: float
   b: float
-  output: float
+  return: numpy float
   '''
   print('The hypotenuse of a right-angled triangle with side lengths of {0:.2f} and {1:.2f} is {2:.2f}'.format(a, b, np.sqrt((a**2) + (b**2))))
   return  np.sqrt((a**2) + (b**2))

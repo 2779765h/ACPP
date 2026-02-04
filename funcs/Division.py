aacc0e7b4df59d4aba27677104b122e84f3681e5
Alicja <2779765h@student.gla.ac.uk>
2026-02-04 12:26:23 +0000

    Update Division.py
    Specifying data types

--- funcs/Division.py
+++ funcs/Division.py
@@ -1,5 +1,11 @@
 def divide(a,b):
-  '''function which divides two numbers (a,b) and outputs the result (a/b)'''
+  '''
+  function which divides two numbers (a,b) and outputs the result (a/b)
+
+  data types:
+  a: int
+  b: int
+  '''
   return a/b
 
 print('Division of {0} / {1} gives {2}'.format(a, b, divide(a,b)))

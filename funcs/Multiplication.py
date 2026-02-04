bf23e1bd4ffe38e8eab5898df91ddb37ce2e3b67
Alicja <2779765h@student.gla.ac.uk>
2026-02-04 12:28:46 +0000

    Update Multiplication.py
    Specifying data types

--- funcs/Multiplication.py
+++ funcs/Multiplication.py
@@ -1,5 +1,12 @@
 def product(a,b):
-  '''function multiplies two numbers (a*b) and outputs the product'''
+  '''
+  function multiplies two numbers (a*b) and outputs the product
+  
+  data types:
+  a: float
+  b: float
+  output: float
+  '''
   return a*b
 
 print('Multiplying {0} x {1} gives {2}'.format(a, b, product(a,b)))

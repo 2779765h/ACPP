3895d29775b5c962c53d1434e024385a3aa18e07
Alicja <2779765h@student.gla.ac.uk>
2026-02-04 21:58:12 +0000

    Create DBScanPlotting.py

---
+++ BasicML/DBScanPlotting.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import DBSCAN
+from colorsys import hls_to_rgb, hsv_to_rgb
+# Colours
+colours = []
+r = 60 # number of clusters / samples
+d = 5
+for n in range(r):
+    h, v = int(n/d), n%d
+    H = h/(r/d)
+    V = (4+v)/8
+    S = 1
+    c = hsv_to_rgb(H,S,V)
+    colours += [c] 
+
+# DBScan
+db = DBSCAN(eps=0.3,min_samples=r).fit(data[:,:2])
+db.labels_
+
+# Plot
+plt.figure(figsize=[10,10])
+plt.title('DBScan Clustering')
+
+for i in range(r):   
+    Cluster = data[np.where(db.labels_ == i)]
+    scatter = plt.scatter(Cluster[:, 0], Cluster[:, 1], color=colours[i], label = i)
+    
+    for n in range(0,Cluster.shape[0],4000): # number of iterations 
+        plt.text(
+            Cluster[n, 0], 
+            Cluster[n, 1]+3, 
+            int(i),
+            horizontalalignment='center'
+        )
+    plt.xlabel("Qx")
+    plt.ylabel("Qy")

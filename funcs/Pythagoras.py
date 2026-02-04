175c0d64a48a510ad6366d787a4dbe9170ce14d2
Alicja <2779765h@student.gla.ac.uk>
2026-02-04 12:34:28 +0000

    Update Pythagoras.py

--- funcs/Pythagoras.py
+++ funcs/Pythagoras.py
@@ -1,7 +1,11 @@
 import numpy as np
 
 def hypotenuse(a,b):
-  "Function calculates the hypotenuse of a right angled triangle"
+  '''
+  Function calculates the hypotenuse of a right angled triangle
+  
+  
+  '''
   return np.sqrt((a**2) + (b**2))
 
 print('The hypotenuse of a right-angled triangle with side lengths of {0} and {1} is {2:.f}'.format(a, b, hypotenuse(a,b)))

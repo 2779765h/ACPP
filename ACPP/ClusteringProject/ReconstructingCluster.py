fed117f9caa9825a7371c661d4a48b881a49921d
Alicja <2779765h@student.gla.ac.uk>
2026-02-07 18:08:35 +0000

    Update and rename KMeansReconstructed.py to ReconstructingCluster.py
    Updating from KMeans to be more general and allows for other clustering algorithms

--- ACPP/ClusteringProject/ReconstructingCluster.py
+++ ACPP/ClusteringProject/ReconstructingCluster.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
 
-def ReconstructedImage(ClusterIndex, Title):
+def ReconstructedImage(ClusterType, ClusterIndex, Title):
     '''
-    Function for plotting the image of a clustered pixel in the real space from reciprocal space
+    Function for plotting the image of a clustered pixel in the real space from reciprocal space.
+    Cluster types extract the label from the specified clustering algorithm.
 
     Data types:
+    ClusterType: String (if db = DBSCAN, then enter db, if k = KMEANS, then k etc.)
     ClusterIndex: Int
     Title: String
     '''
@@ -14,7 +15,7 @@ def ReconstructedImage(ClusterIndex, Title):
     Qxmax, Qymax = data[:,3].max().astype('int')+1, data[:,4].max().astype('int')+1
 
     # Select a specific cluster points
-    Cluster = data[np.where(k.labels_ == ClusterIndex)]
+    Cluster = data[np.where(ClusterType.labels_ == ClusterIndex)]
 
     # Create image 
     image = np.zeros(shape=(Qxmax,Qymax))

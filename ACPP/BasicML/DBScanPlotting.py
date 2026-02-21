d1d0df2940d8b18abeef9c338333aa309f5e5936
Alicja <2779765h@student.gla.ac.uk>
2026-02-21 23:30:57 +0000

    Update DBScanPlotting.py
    change iteration number from 4000 to 4500

--- ACPP/BasicML/DBScanPlotting.py
+++ ACPP/BasicML/DBScanPlotting.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 from colorsys import hls_to_rgb, hsv_to_rgb
+
 # Colours
 colours = []
 r = 60 # number of clusters / samples
@@ -31,7 +32,7 @@ for i in range(r):
     Cluster = data[np.where(db.labels_ == i)]
     scatter = plt.scatter(Cluster[:, 0], Cluster[:, 1], color=colours[i], label = i)
     
-    for n in range(0,Cluster.shape[0],4000): # number of iterations 
+    for n in range(0,Cluster.shape[0],4500): # number of iterations 
         plt.text(
             Cluster[n, 0], 
             Cluster[n, 1]+3, 

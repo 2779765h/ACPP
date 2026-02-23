213214baa4550bfe571baf74152f956a474b722a
Alicja <2779765h@student.gla.ac.uk>
2026-02-23 10:53:32 +0000

    Update __init__.py

--- ACPP/BasicML/__init__.py
+++ ACPP/BasicML/__init__.py
@@ -1,4 +1,4 @@
-from ACPP.BasicML.DBScanPlotting import *
+from ACPP.BasicML.DBScanPlotting import DBScanClustering
 from ACPP.BasicML.KMeansPlotting import *
 from ACPP.BasicML.MinimizationFunction import minimisefunc
 from ACPP.BasicML.PeriodicSignal import sinfunc

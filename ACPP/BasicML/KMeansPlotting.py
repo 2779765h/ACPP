aef1c669d09fa78e23ef007341947a0a9cdd9ef3
Alicja <2779765h@student.gla.ac.uk>
2026-02-23 10:49:56 +0000

    Update KMeansPlotting.py
    Changing to be a function, so that it works without specifically the dataset in the code.

--- ACPP/BasicML/KMeansPlotting.py
+++ ACPP/BasicML/KMeansPlotting.py
@@ -3,39 +3,48 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from colorsys import hls_to_rgb, hsv_to_rgb
 
-# Colours
-colours = []
-r = 60
-d = 5
-for n in range(r):
-    h, v = int(n/d), n%d
-    H = h/(r/d)
-    V = (4+v)/8
-    S = 1
-    c = hsv_to_rgb(H,S,V)
-    colours += [c] 
+def KMeansClustering(Data, r):
+    '''
+    Function to apply KMeans clustering onto dataset.
 
-# Load Data
-data = np.load('BFBTgrain_112_pointsarray_bin.npy')
-Qx = data[:,0]
-Qy = data[:,1]
+    Data types:
+    Data: np array
+    r: integer
 
-# Apply KMeans
-k = KMeans(n_clusters=r).fit(data[:,:2])
-
-# Plot
-plt.figure(figsize=[10,10])
-plt.title('KMeans Clustering')
-for i in range(r):
-    Cluster = data[np.where(k.labels_ == i)]
-    scatter = plt.scatter(Cluster[:, 0], Cluster[:, 1], color=colours[i], label = i)
+    Return:
+    Matplotlib plot
+    '''
     
-    for n in range(0,Cluster.shape[0],4500): # number of iterations 
-        plt.text(
-            Cluster[n, 0], 
-            Cluster[n, 1]+3, 
-            int(i),
-            horizontalalignment='center'
-        )
-    plt.xlabel("Qx")
-    plt.ylabel("Qy")
+    # Colours
+    colours = []
+    d = 5
+    for n in range(r):
+        h, v = int(n/d), n%d
+        H = h/(r/d)
+        V = (4+v)/8
+        S = 1
+        c = hsv_to_rgb(H,S,V)
+        colours += [c] 
+    
+    # Data
+    Qx = data[:,0]
+    Qy = data[:,1]
+
+    # Apply KMeans
+    k = KMeans(n_clusters=r).fit(data[:,:2])
+
+    # Plot
+    plt.figure(figsize=[10,10])
+    plt.title('KMeans Clustering')
+    
+    for i in range(r):   
+        Cluster = data[np.where(k.labels_ == i)]
+        scatter = plt.scatter(Cluster[:, 0], Cluster[:, 1], color=colours[i], label = i)
+    
+        for n in range(0, Cluster.shape[0], 4500): # number of iterations 
+            plt.text(
+                Cluster[n, 0], 
+                Cluster[n, 1]+3, 
+                int(i),
+                horizontalalignment='center'
+            )

91df724ed721ef470559890c0b5894135b440ff8
Alicja <2779765h@student.gla.ac.uk>
2026-02-23 10:37:27 +0000

    Update DBScanPlotting.py
    Redefnie code to be a function

--- ACPP/BasicML/DBScanPlotting.py
+++ ACPP/BasicML/DBScanPlotting.py
@@ -1,43 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
-from colorsys import hls_to_rgb, hsv_to_rgb
+from colorsys import hsv_to_rgb
 
-# Colours
-colours = []
-r = 60 # number of clusters / samples
-d = 5
-for n in range(r):
-    h, v = int(n/d), n%d
-    H = h/(r/d)
-    V = (4+v)/8
-    S = 1
-    c = hsv_to_rgb(H,S,V)
-    colours += [c] 
-
-# Import Data
-data = np.load('BFBTgrain_112_pointsarray_bin.npy')
-Qx = data[:,0]
-Qy = data[:,1]
-
-# DBScan
-db = DBSCAN(eps=0.310,min_samples=r).fit(data[:,:2])
-db.labels_
-
-# Plot
-plt.figure(figsize=[10,10])
-plt.title('DBScan Clustering')
-
-for i in range(r):   
-    Cluster = data[np.where(db.labels_ == i)]
-    scatter = plt.scatter(Cluster[:, 0], Cluster[:, 1], color=colours[i], label = i)
+def DBScan(Data, r):
     
-    for n in range(0,Cluster.shape[0],4500): # number of iterations 
-        plt.text(
-            Cluster[n, 0], 
-            Cluster[n, 1]+3, 
-            int(i),
-            horizontalalignment='center'
-        )
-    plt.xlabel("Qx")
-    plt.ylabel("Qy")
+    # Colours
+    colours = []
+    d = 5
+    for n in range(r):
+        h, v = int(n/d), n%d
+        H = h/(r/d)
+        V = (4+v)/8
+        S = 1
+        c = hsv_to_rgb(H,S,V)
+        colours += [c] 
+    
+    # Import Data
+    # data = np.load('BFBTgrain_112_pointsarray_bin.npy')
+    Qx = data[:,0]
+    Qy = data[:,1]
+
+    # DBScan
+    db = DBSCAN(eps=0.30, min_samples=r).fit(data[:,:2])
+    db.labels_
+
+    # Plot
+    plt.figure(figsize=[10,10])
+    plt.title('DBScan Clustering')
+
+    for i in range(r):   
+        Cluster = data[np.where(db.labels_ == i)]
+        scatter = plt.scatter(Cluster[:, 0], Cluster[:, 1], color=colours[i], label = i)
+    
+        for n in range(0,Cluster.shape[0],4500): # number of iterations 
+            plt.text(
+                Cluster[n, 0], 
+                Cluster[n, 1]+3, 
+                int(i),
+                horizontalalignment='center'
+            )
+        plt.xlabel("Qx")
+        plt.ylabel("Qy")

351aebcb4c493795bd1c5ad7d52e162fe8e0803b
Alicja <2779765h@student.gla.ac.uk>
2026-03-11 22:05:39 +0000

    Update ClusterCOM.py

--- ACPP/ClusteringProject/ClusterCOM.py
+++ ACPP/ClusteringProject/ClusterCOM.py
@@ -2,35 +2,45 @@ import numpy as np
 
 def COM_X(m,x):
     '''
-    Calculates COM for x-coordinate.
+    Calculates COM for x-coordinate
 
     Data types:
     m: int or numpy array
     x: numpy array
+    return: float
     '''
     return (np.sum(m * x)) / np.sum(m)
 
 def COM_Y(m,y):
     '''
-    Calculates COM for y-coordinate.
+    Calculates COM for y-coordinate
 
     Data types:
     m: int or numpy array
     y: numpy array
+    return float
     '''
     return (np.sum(m * y)) / np.sum(m)
 
-def Cluster_COM(Data, Range, label, Weight=True):
+def Cluster_COM(Data, Range, label, c1, c2, c3, Weight=True):
     '''
-    Calculates COM for every cluster in a clustering graph.
+    Calculates COM for every cluster in a clustering graph
     
     Data types:
     Data: numpy array
     Range: numpy array
     label: numpy array
+        this corresponds to the clustering type, so for DBSCAN use db.labels_
+
+    c1: int
+    c2: int
+    c3: int
+        c indicates the column index
+        
     Weight: bool
         if True, applies pixel weighting to the centre of masses
 
+    return: list
     '''
     COM_Values = []
 
@@ -41,15 +51,15 @@ def Cluster_COM(Data, Range, label, Weight=True):
         if Weight:
             # COM weighting
             COM_Values.append((
-                COM_X(Cluster[:,2], Cluster[:,3]),
-                COM_Y(Cluster[:,2], Cluster[:,4])
+                COM_X(Cluster[:,c1], Cluster[:,c2]),
+                COM_Y(Cluster[:,c1], Cluster[:,c3])
             ))
 
         else:
             #COM no weighting
             COM_Values.append((
-                COM_X(1, Cluster[:,3]),
-                COM_Y(1, Cluster[:,4])
+                COM_X(1, Cluster[:,c2]),
+                COM_Y(1, Cluster[:,c3])
             ))
             
     return COM_Values

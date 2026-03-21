ac418676e1c4c7b6b3b4ce0631d45135bd3debd7
Alicja <2779765h@student.gla.ac.uk>
2026-03-21 13:14:49 +0000

    Update PeriodicSignal.py

--- BasicML/PeriodicSignal.py
+++ BasicML/PeriodicSignal.py
@@ -2,18 +2,23 @@ import numpy as np
 
 def sinfunc(t, a, b, c, d, t1, t2, t3):
     '''
-    Data types:
-    t: np.float
-    a: float
-    b: float
-    c: float
-    d: float
-    t1: np.float
-    t2: np.float
-    t2: np.float
-    return: np.array
-
     This function calculates a periodic signal f(t) based on the linear combination of functions of the form:
-    f(t) = a*sin(t-t1) + bsin(2t-t2) + csin(3t-t3) + 1
+    f(t) = a*sin(t-t1) + bsin(2t-t2) + csin(3t-t3) + 1.
+
+    Parameters
+    ----------
+    t: numpy float
+        defines the range of the period
+    a, b, c: float
+        constants for sine amplitude
+    t1, t2, t3: numpy float
+        arbitrary angle values
+
+    Returns
+    ----------
+    f: numpy array
+        linear combination of periodic functions
+
     '''
-    return (a*np.sin(t - t1)) + (b*np.sin(2*t - t2)) + (c*np.sin(3*t - t3)) + (d*1)
+    f = (a*np.sin(t - t1)) + (b*np.sin(2*t - t2)) + (c*np.sin(3*t - t3)) + (d*1)
+    return f

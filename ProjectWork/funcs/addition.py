9f526e33771d0432fa5bd442647929dab531630d
Alicja <2779765h@student.gla.ac.uk>
2026-03-22 11:25:18 +0000

    Update addition.py

--- ProjectWork/funcs/addition.py
+++ ProjectWork/funcs/addition.py
@@ -1,11 +1,16 @@
 def add(a,b):
   '''
-  function to add two numbers (a,b) and output the sum (a + b)
-  
-  data types:
+  Function to add two numbers (a,b) and output the sum (a + b)
+      
+  Parameters
+  ----------
   a: float
   b: float
-  return: float
+        
+  Returns
+  ----------
+  float
+      sum of a + b
   '''
   print('Addition of {0} + {1} is {2}'.format(a, b, a + b))
   return a + b

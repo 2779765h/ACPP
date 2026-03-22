04ce11296991f1d2fcf757823d6e132595cadf57
Alicja <2779765h@student.gla.ac.uk>
2026-03-22 11:33:30 +0000

    Update multiplication.py

--- ProjectWork/funcs/multiplication.py
+++ ProjectWork/funcs/multiplication.py
@@ -1,11 +1,18 @@
 def product(a,b):
   '''
-  function multiplies two numbers (a*b) and outputs the product
-  
-  data types:
+  Function which multiplies two numbers (a*b) and outputs the product
+
+  Parameters
+  ----------
   a: float
+    input number
   b: float
-  return: float
+    input number
+        
+  Returns
+  ----------
+  float
+      number from the result of the product of a*b
   '''
   print('Multiplying {0} x {1} gives {2}'.format(a, b, a*b))
   return a*b

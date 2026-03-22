5341187d01caa0ccba2b2d13d5b5494e2e1edb68
Alicja <2779765h@student.gla.ac.uk>
2026-03-22 11:31:03 +0000

    Update division.py

--- ProjectWork/funcs/division.py
+++ ProjectWork/funcs/division.py
@@ -1,11 +1,16 @@
 def divide(a,b):
   '''
-  function which divides two numbers (a,b) and outputs the result (a/b)
-
-  data types:
+  Function which divides two numbers (a,b) and outputs the result (a/b)
+  
+  Parameters
+  ----------
   a: float
   b: float
-  return: float
+        
+  Returns
+  ----------
+  float
+      number from the result of the division of a/b
   '''
   print('Division of {0} / {1} gives {2}'.format(a, b, a/b))
   return a/b
